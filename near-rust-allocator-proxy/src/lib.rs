5053bc5a369e605495d8b9b383573ac3f765d4cd
Piotr Mikulski <piotr@near.org>
2021-12-03 22:42:25 -0800

    Add denies to `lib.rs`

--- near-rust-allocator-proxy/src/lib.rs
+++ near-rust-allocator-proxy/src/lib.rs
@@ -1,2 +1,12 @@
-#![deny(unused_crate_dependencies, unused_extern_crates, variant_size_differences)]
+#![deny(
+    unused_crate_dependencies,
+    unused_extern_crates,
+    variant_size_differences
+)]
 pub mod allocator;
+
+/// Used by benches;
+#[cfg(test)]
+use criterion as _;
+#[cfg(test)]
+use tikv_jemallocator as _;
