d315a91b29e673016a6e73fb64a5d2f6ab971c73
Piotr Mikulski <piotr@near.org>
2021-12-15 23:51:28 -0800

    Refactor benches

--- near-rust-allocator-proxy/benches/allocations.rs
+++ near-rust-allocator-proxy/benches/allocations.rs
@@ -6,25 +6,16 @@ static ALLOC: MyAllocator<tikv_jemallocator::Jemalloc> =
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn small_alloc_test(n: usize) -> usize {
-    let mut x = Vec::<usize>::new();
-    x.resize(n, 0);
-    let mut sum = 0;
-    for val in x.iter() {
-        sum += val;
-    }
-    sum
+fn small_alloc_benchmark(c: &mut Criterion) {
+    c.bench_function("small_alloc_benchmark", |b| {
+        b.iter(|| {
+            black_box(Vec::<usize>::with_capacity(128));
+        })
+    });
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("alloc test", |b| b.iter(|| small_alloc_test(black_box(20))));
-}
-
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, small_alloc_benchmark);
 criterion_main!(benches);
 /*
-/// without #[global_allocator]
-alloc test              time:   [22.098 ns 22.180 ns 22.259 ns]
-/// with #[global_allocator]
-alloc test              time:   [55.122 ns 55.707 ns 56.387 ns]
+small_alloc_benchmark   time:   [1.8438 us 1.8477 us 1.8528 us]
  */
