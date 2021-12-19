66c09fa9d31bf406af8d7ee59a3d53d5418029fb
Piotr Mikulski <piotr@near.org>
2021-12-19 00:09:59 -0800

    Add missing functionality

--- near-rust-allocator-proxy/src/lib.rs
+++ near-rust-allocator-proxy/src/lib.rs
@@ -4,7 +4,7 @@ mod config;
 pub use allocator::{
     current_thread_memory_usage, current_thread_peak_memory_usage, print_memory_stats,
     reset_memory_usage_max, thread_memory_count, thread_memory_usage, total_memory_usage,
-    ProxyAllocator,
+    AllocHeader, ProxyAllocator, ALLOC_HEADER_SIZE,
 };
 
 pub use config::AllocatorConfig;
