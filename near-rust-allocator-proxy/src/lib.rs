3707fe987141f0d922e7c925593340583939d86f
Piotr Mikulski <piotr@near.org>
2021-12-20 14:09:51 -0800

    Rewrite code handling alignment

--- near-rust-allocator-proxy/src/lib.rs
+++ near-rust-allocator-proxy/src/lib.rs
@@ -4,7 +4,7 @@ mod config;
 pub use allocator::{
     current_thread_memory_usage, current_thread_peak_memory_usage, print_memory_stats,
     reset_memory_usage_max, thread_memory_count, thread_memory_usage, total_memory_usage,
-    AllocHeader, ProxyAllocator, ALLOC_HEADER_SIZE,
+    AllocHeader, ProxyAllocator,
 };
 
 pub use config::AllocatorConfig;
